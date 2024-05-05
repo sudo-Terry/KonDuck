c951d929ac6f4fea7fa918c199e90f730e6d47fa
Kihoon Kim <contact.terrykim@gmail.com>
2024-05-05 23:58:54 +0900

    webpacker ReactJS

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const babelLoader = environment.loaders.get('babel')
+babelLoader.use.push({
+  loader: 'babel-loader',
+  options: {
+    presets: ['@babel/preset-react']
+  }
+})
+
 module.exports = environment
