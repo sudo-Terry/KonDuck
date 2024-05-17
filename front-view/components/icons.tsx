58111f1f2a71657b8b9a52fe52044b0d4cc51ac6
Kihoon Kim <contact.terrykim@gmail.com>
2024-05-17 16:17:21 +0900

    REFACTOR - Icon 컴포넌트 분리
    ### TODO
    - DevAC 아이콘 호스팅 이후, 로고 컴포넌트 변경 필요

---
+++ front-view/components/icons.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type IconProps = React.SVGProps<SVGSVGElement>;
+
+export function SearchIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>
+  );
+}
+
+export function MountainIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 2L2 22h20L12 2z" />
+      <path d="M12 6l4 14H8l4-14z" />
+    </svg>
+  );
+}
--- front-view/pages/index.jsx
+++ front-view/pages/index.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ArticleCard } from "@/components/ArticleCard";
+import { SearchIcon, MountainIcon } from "@/components/icons";
 
 export default function Component() {
   return (
@@ -196,45 +197,5 @@ export default function Component() {
         </div>
       </footer>
     </>
-  );
-}
-
-function SearchIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
-    </svg>
-  );
-}
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 2L2 22h20L12 2z" />
-      <path d="M12 6l4 14H8l4-14z" />
-    </svg>
   );
 }
