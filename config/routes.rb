25b87695cfc6be125a39ea3c7760cfb3a529b87f
BOOK-G8Q4HDAMDT\gangh <ganghyeon5297@naver.com>
2024-05-17 17:38:08 +0900

    댓글 달기 기능,댓글저장용 comment 모델

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   get 'home', to: 'home#index'
   get 'qna', to: 'qna#index'
   get 'board', to: 'board#index'
 
-  resources :board, only: [:index, :create, :show, :new , :destroy]
+  resources :board, only: [:index, :create, :show, :new , :destroy]do
+    resources :comments, only: :create
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_15_072915) do
+ActiveRecord::Schema.define(version: 2024_05_17_081150) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2024_05_15_072915) do
     t.integer "company_id"
     t.string "url"
     t.index ["company_id"], name: "index_articles_on_company_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -38,4 +46,5 @@ ActiveRecord::Schema.define(version: 2024_05_15_072915) do
   end
 
   add_foreign_key "articles", "companies"
+  add_foreign_key "comments", "posts"
 end
