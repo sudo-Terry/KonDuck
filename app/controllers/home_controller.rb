f9ffe8f5165bb05604e976ebeff9a593c933f6c5
BOOK-G8Q4HDAMDT\gangh <ganghyeon5297@naver.com>
2024-05-23 16:53:19 +0900

    crud 중에서 r빼고 json으로 렌더링

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,27 @@ class HomeController < ApplicationController
 
     render json: @articles
   end
+
+  def create 
+    @articles =Article.new(article_params)
+    @articles.save!
+    render json: @articles
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @articles
+  end
+    
+  def destroy
+    @article.destroy
+  end
+
+    
+  
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text, :url, :company_id)
+  end
 end
