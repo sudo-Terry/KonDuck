613a6e50789882cc545ba96c51e0c653a5e2fd65
S1MPLKH <ganghyeon5297@naver.com>
2024-05-31 02:53:25 +0900

    포스트에서 카테고리 칼럼 삭제

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_28_164829) do
+ActiveRecord::Schema.define(version: 2024_05_30_173144) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2024_05_28_164829) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_name"
