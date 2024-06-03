e530335335d557242aab105431c481c921123f94
Kihoon Kim <contact.terrykim@gmail.com>
2024-06-03 13:38:13 +0900

    구글, 엔비디아, 네이버 프로필 이미지 대응
    - 프로필 칸에 Svg이미지 노출
      - Google AI
      - Naver D2
      - Nvidia
    - enum 추가
    - 홈페이지 탭 아이콘 추가

--- front-view/enums/CompanyTypes.ts
+++ front-view/enums/CompanyTypes.ts
@@ -1,11 +1,23 @@
 export enum CompanyTypes {
   Kakao = 1,
   Netflix = 2,
-  Woowahan = 3,
+  //Woowahan = 3,
+  Google_AI = 4,
+  Google_Mobile = 5,
+  Google_Web_Cloud = 6,
+  Nvidia_ComputerVision = 7,
+  Nvidia_Cloud = 8,
+  Naver_D2 = 9,
 }
 
 export const CompanyTypesLabel: { [key in CompanyTypes]: string } = {
   [CompanyTypes.Kakao]: "Kakao",
   [CompanyTypes.Netflix]: "Netflix",
-  [CompanyTypes.Woowahan]: "우아한형제들",
+  //[CompanyTypes.Woowahan]: "우아한형제들",
+  [CompanyTypes.Google_AI]: "Google AI",
+  [CompanyTypes.Google_Mobile]: "Google Mobile",
+  [CompanyTypes.Google_Web_Cloud]: "Google Web Cloud",
+  [CompanyTypes.Nvidia_ComputerVision]: "Nvidia Computer Vision",
+  [CompanyTypes.Nvidia_Cloud]: "Nvidia Cloud",
+  [CompanyTypes.Naver_D2]: "Naver D2",
 };
