489c0385b92b3de21a55b66f33b0a6bde997e478
S1MPLKH <ganghyeon5297@naver.com>
2024-06-03 16:28:11 +0900

    answer 컨트롤러,라우팅 설정

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get 'home', to: 'home#index'
-    resources :qna, only: [:index, :new, :create, :show, :update, :destroy] 
+    resources :qna, only: [:index, :show, :create, :update, :destroy] do
+      resources :qna_answers, only: [:index, :show, :create, :update, :destroy]
+    end
     resources :board, only: [:index, :new, :create, :show, :update, :destroy] do
       resources :post_comments, only: [:index, :new, :create, :show, :update, :destroy]
     end
